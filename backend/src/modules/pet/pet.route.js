3767b14c7cf592f4aaf13b0912de2985f9d65741
LizaNotseagull <n.elizaveta2004@gmail.com>
2026-05-31 00:00:00 +0500

    Update pet.route.js

--- backend/src/modules/pet/pet.route.js
+++ backend/src/modules/pet/pet.route.js
@@ -1,10 +1,16 @@
-import express from 'express'
+import express from 'express';
 import * as petController from './pet.controller.js';
+import { feedPet, playPet, dailyBonus, healPet } from '../controllers/pet.controller.js';
 
 export const petRouter = express.Router();
 
 petRouter.get('/', petController.getPet);
-
 petRouter.post('/:id/complete', petController.getPet);
-
 petRouter.post('/update', petController.updatePet);
+
+petRouter.post('/feed', feedPet);
+petRouter.post('/play', playPet);
+petRouter.post('/daily', dailyBonus);
+petRouter.post('/heal', healPet);
+
+export default petRouter;
