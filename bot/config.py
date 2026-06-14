07ab8c3a8e8be5bbb416071eff3073e1f76dc974
LizaNotseagull <n.elizaveta2004@gmail.com>
2026-06-15 00:31:49 +0500

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -5,6 +5,6 @@ load_dotenv('.env.bot')
 
 TELEGRAM_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
 if not TELEGRAM_TOKEN:
-    TELEGRAM_TOKEN = ""
+    TELEGRAM_TOKEN = "Ваш токен"
 
-BACKEND_URL = os.getenv("BACKEND_URL", "http://localhost:3000")
+ADMIN_IDS = [int(x.strip()) for x in os.getenv("ADMIN_IDS", "").split(",") if x.strip()]
